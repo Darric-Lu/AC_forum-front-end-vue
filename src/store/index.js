794a9204dc3e7de4ce687472e86dc647256d7297
Darric <darric.lu@gmail.com>
2021-03-01 14:18:01 +0800

    Signin data & keep status

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import userAPI from '../apis/users'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false
+    },
+    isAuthenticated: false
   },
+  //commit 發動 action
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser
+      }
+      // 將使用者的登入狀態改為 true
+      state.isAuthenticated = true
+    }
   },
+  //dispatch 發動 action
   actions: {
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data } = await userAPI.getCurrentUser()
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        const { id, name, email, image, isAdmin } = data
+        commit('setCurrentUser', {
+          id, name, email, image, isAdmin
+        })
+        console.log('data', data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    },
   },
   modules: {
-  }
+  },
 })
